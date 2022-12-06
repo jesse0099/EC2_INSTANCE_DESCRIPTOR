2d13b63757181ab16cd6f59602c76f32acd400cd
CHAVEZ RIVAS <45377464+jesse0099@users.noreply.github.com>
2022-12-06 12:06:19 -0600

    Delete envs from repo, added to git  ignore.

--- app/envs.py
+++
@@ -1,9 +0,0 @@
-
-class Environment_varibles:
-    """Environment variables to control client behavior.
-    """
-    # replace with own values
-    AIRTABLE_API_KEY = "replace with own value"
-    AIRTABLE_BASE_ID = "replace with own value"
-    EC2_INSTANCES_TID = "replace with own value"
-    EC2_SECURITY_GROUPS_TID = "replace with own value"
